bc6efdba20f850196ee154eba8a07861d9d3708a
YilanCemal <cemalyigit123@hotmail.com>
2023-04-16 21:40:56 +0300

    eeh bişeyler bişeyler

---
+++ Zone.py
@@ -0,0 +1,71 @@
+import discord
+import logging
+import os
+from dotenv import load_dotenv
+from discord.utils import get
+from pathlib import Path
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+
+client = discord.Client(intents=intents)
+
+played_sounds = []
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name='/yardım'))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('/lan'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('/gellan'):
+        if message.author.voice:
+            channel = message.author.voice.channel
+            vc = await channel.connect()
+            if vc not in played_sounds:
+                sound_path = Path('sounds/Zone.mp3')  
+                if sound_path.exists():
+                    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(str(sound_path)))
+                    vc.play(source)
+                    played_sounds.append(vc)
+            else:
+                # inform the user that the sound has already been played
+                await message.channel.send('The sound has already been played.')
+        else:
+            await message.channel.send("You're not in a voice channel.")
+
+    elif message.content.startswith('/gaybol'):
+        for vc in client.voice_clients:
+            if vc.guild == message.guild:
+                await vc.disconnect()
+                # remove the voice client from the list of played sounds
+                if vc in played_sounds:
+                    played_sounds.remove(vc)
+                break
+
+    elif message.content.startswith('/yardım'):
+        # display a help message
+        help_message = '''
+        `/gellan` - kanala gelir
+        `/gaybol` - kanaldan gider
+        `/yardım` - yardım gösterir
+        '''
+        await message.channel.send(help_message)
+
+client.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
 py -3 -m pip install -U discord.py
 pip install python-dotenv
-pip install pynacl 
+pip install pynacl
+pip install asyncio   
+
+py -3 Zone.py (botu çalıştırmak için)
